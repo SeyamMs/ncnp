f69c60579516a1fc6e122cddf778fe7afd10c994
Mohammed Seyam <seyam.ms@gmail.com>
2025-01-24 22:27:58 +0300

    write configurations files

--- config.php
+++ config.php
@@ -1,9 +1,24 @@
 <?php
 
 return [
+
     'production' => false,
-    'baseUrl' => '',
-    'title' => 'Jigsaw',
-    'description' => 'Website description.',
-    'collections' => [],
+    'baseUrl' => $baseUrl = 'http://localhost:8000',
+
+    'title' => 'المركز الوطني لتنمية القطاع غير الربحي',
+    'description' => 'تحدي الابتكار للقطاع غير الربحي يهدف إلى تحفيز الابتكار واستقطاب حلول مبتكرة لتحسين تجربة ضيوف الرحمن وتعزيز استدامة المنظمات غير الربحية، مع إحداث أثر اجتماعي مستدام عبر مراحل مدروسة.',
+    'keywords' => [
+        'تحدي الابتكار',
+        'لمنظمات القطاع غير الربحي ابتكر في خدمات ومنتجات ضيوف الرحمن',
+        'المركز الوطني لتنمية القطاع غير الربحي',
+    ],
+
+    'collections' => [
+        'paths' => [
+            'path' => 'paths',
+        ]
+    ],
+
+    'asset' => fn($page, $source) => $baseUrl . str($source)->start('/assets/'),
+
 ];
--- config.production.php
+++ config.production.php
@@ -1,5 +1,9 @@
 <?php
 
 return [
+
     'production' => true,
+    'baseUrl' => $baseUrl = 'https://electrathon-sera.com',
+    'asset' => fn($page, $source) => $baseUrl . str($source)->start('/assets/'),
+
 ];
